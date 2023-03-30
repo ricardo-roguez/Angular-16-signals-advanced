9a0bbb77a2778aab9d144d64d1887ccf300e9aad
ricardo-roguez <120378561+ricardo-roguez@users.noreply.github.com>
2023-03-30 17:02:14 +0200

    update logic

--- src/app-component.ts
+++ src/app-component.ts
@@ -3,8 +3,8 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Manager de Facturas</h1>
     <div class='main-component'>
+      <h1>Manager de Facturas</h1>
       <router-outlet></router-outlet>
     </div>
   `,
--- src/components/invoice-list/invoice-list.component.ts
+++ src/components/invoice-list/invoice-list.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit, Signal } from '@angular/core';
+import { Component, computed, inject, OnInit, SettableSignal, Signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
+import { Invoice } from '../../interfaces/invoice';
 import { InvoiceService } from '../../services/invoice.service';
 import { InvoiceItemComponent } from '../invoice-item/invoice-item.component';
 
@@ -18,20 +19,24 @@ export class InvoiceListComponent implements OnInit {
   private invoiceService = inject(InvoiceService);
   private invoicesInDoneSignabble = computed(() => this.invoiceListArray().filter((item) => item.status === 'DONE'));
   
-  invoiceListArray = this.invoiceService.invoiceList;
+  invoiceListArray: SettableSignal<Invoice[]>;
   finishedCount!: Signal<number>;
   totalAmount!: Signal<number>;
 
   ngOnInit() {
-    this.activatedRoute.queryParams
-      .pipe(tap(data => console.log(data)))
+    this.activatedRoute.params
+      .pipe(
+        map(data => data.id),
+        switchMap((id) => this.invoiceService.getInvoicesByClient(id)),
+        tap((data) => this.invoiceListArray = data)
+      )
       .subscribe()
 
     this.finishedCount = computed(() => this.invoicesInDoneSignabble().length);
     this.totalAmount = computed(() => this.invoicesInDoneSignabble().reduce((accumulator, invoice) =>  accumulator + invoice.amount, 0))
   }
 
-  backToClientList() {
+  backToClientList(): void {
     this.route.navigate(['/'])
   }
 }
