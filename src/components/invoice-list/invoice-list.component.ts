07887b06d5490ef0480af224a0485568289d1990
ricardo-roguez <120378561+ricardo-roguez@users.noreply.github.com>
2023-03-30 15:15:42 +0200

    get params from route

--- src/components/invoice-list/invoice-list.component.ts
+++ src/components/invoice-list/invoice-list.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, inject, OnInit, Signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs';
 import { InvoiceService } from '../../services/invoice.service';
 import { InvoiceItemComponent } from '../invoice-item/invoice-item.component';
 
@@ -11,15 +13,25 @@ import { InvoiceItemComponent } from '../invoice-item/invoice-item.component';
   styleUrls: ['./invoice-list.component.css'],
 })
 export class InvoiceListComponent implements OnInit {
+  private route = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
   private invoiceService = inject(InvoiceService);
-  private invoicesInDoneSignabble = computed(() =>  this.invoiceListArray().filter((item) => item.status === 'DONE'));
+  private invoicesInDoneSignabble = computed(() => this.invoiceListArray().filter((item) => item.status === 'DONE'));
   
   invoiceListArray = this.invoiceService.invoiceList;
   finishedCount!: Signal<number>;
   totalAmount!: Signal<number>;
 
   ngOnInit() {
+    this.activatedRoute.queryParams
+      .pipe(tap(data => console.log(data)))
+      .subscribe()
+
     this.finishedCount = computed(() => this.invoicesInDoneSignabble().length);
     this.totalAmount = computed(() => this.invoicesInDoneSignabble().reduce((accumulator, invoice) =>  accumulator + invoice.amount, 0))
+  }
+
+  backToClientList() {
+    this.route.navigate(['/'])
   }
 }
