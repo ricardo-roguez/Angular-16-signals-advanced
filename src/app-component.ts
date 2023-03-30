b6399deed2c6648f8950c8b31e55b823ca738514
ricardo-roguez <120378561+ricardo-roguez@users.noreply.github.com>
2023-03-30 14:59:39 +0200

    3 files modified

--- src/app-component.ts
+++ src/app-component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Testing Signals!</h1>
+    <h1>Manager de Facturas</h1>
     <div class='main-component'>
       <router-outlet></router-outlet>
     </div>
